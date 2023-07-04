242163e1dac3db1fed82fb3f14694586caae9722
alessandro-001 <alessandrofrondini@gmail.com>
2023-07-04 09:55:46 +0100

    fixes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import BannerLogoDark from '../../src/assets/banner-logo.png'
 
 export default function Banner() {
   return (
-    <div className='py-5'>
+    <div id='banner' className='py-5'>
         <img 
             src={BannerLogoDark} 
             alt='banner-logo'></img>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <div className="flex justify-center mb-10">
-        <a href="#top-page" className="inline-block mx-5 px-8 py-4 border border-slate-900 dark:border-[#13ecd4] text-base hover:text-white dark:text-[#13ecd4] font-medium rounded-md text-slate-900 hover:bg-slate-900 dark:hover:border-pink-500 md:text-md dark:hover:text-pink-500">Back to the top!</a>
+        <a href="#banner" className="inline-block mx-5 px-8 py-4 border border-slate-900 dark:border-[#13ecd4] text-base hover:text-white dark:text-[#13ecd4] font-medium rounded-md text-slate-900 hover:bg-slate-900 dark:hover:border-pink-500 md:text-md dark:hover:text-pink-500">Back to the top!</a>
       </div>
       <div className='py-5 bg-slate-300 dark:bg-slate-400 text-slate-900 text-center rounded-t-3xl'>
         <p>
