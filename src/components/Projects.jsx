f2b9623e7fff5e8ae6bdebd1ab0e4c1a22f9d422
alessandro-001 <alessandrofrondini@gmail.com>
2023-09-04 12:47:08 +0100

    added carousel for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import SectionTitle from './SectionTitle'
-import ProjectItem from './ProjectItem'
-import works from '../data/projects'
+import React from 'react';
+import SectionTitle from './SectionTitle';
+import ProjectItem from './ProjectItem';
+import works from '../data/projects';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 export default function Projects() {
-  return (
-    <div className='py-10'>
+    const sliderSettings = {
+      arrows: true,
+      infinite: true,
+      speed: 1000, 
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000, 
+      cssEase: 'linear',
+      dots: true,
+    };
+  
+    return (
+      <div className='py-10'>
         <SectionTitle id='projects'>My Projects</SectionTitle>
-        <div>
-            {works.map(work => {
-                return(
-                    <ProjectItem 
-                        key={work.title}
-                        imgUrl={work.imgUrl}
-                        title={work.title}
-                        tech={work.tech}
-                        description={work.description}
-                        repolink={work.repolink}
-                        weblink={work.weblink}
-                    ></ProjectItem>
-                )
-            })}
-        </div>
-    </div>
-  )
-}
+        <Slider {...sliderSettings}>
+          {works.map((work, index) => {
+            return (
+              <div key={index} className='project-slide'>
+                <ProjectItem
+                  imgUrl={work.imgUrl}
+                  title={work.title}
+                  tech={work.tech}
+                  description={work.description}
+                  repolink={work.repolink}
+                  weblink={work.weblink}
+                ></ProjectItem>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    );
+  }
+  
