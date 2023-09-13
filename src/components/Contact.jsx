a614052aba845680e23f7682f13c781f209f66eb
alessandro-001 <alessandrofrondini@gmail.com>
2023-09-13 14:56:04 +0100

    improved UX

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,32 +34,32 @@ export default function Contact() {
     </div>
     <div className="py-10 flex justify-center">
       <form ref={form} onSubmit={sendEmail} className="flex flex-col w-full sm:w-1/2">
-        <div className="flex flex-col mb-4 py-2 dark:text-[#13ecd4]">
+        <div className="flex flex-col mb-4 py-2 dark:text-[#13ecd4] hover:dark:text-pink-500">
           <label htmlFor="user_name" className="mb-1">Name:</label>
           <input
             type="text"
             name="user_name"
             id="user_name"
-            className="border border-gray-300 dark:border-[#13ecd4] px-3 py-2 rounded-md text-slate-900 w-full"
+            className="border border-gray-300 dark:border-[#13ecd4] dark:hover:border-pink-500 px-3 py-2 rounded-md text-slate-900 w-full"
             required
           />
         </div>
-        <div className="flex flex-col mb-4 py-2 dark:text-[#13ecd4]">
+        <div className="flex flex-col mb-4 py-2 dark:text-[#13ecd4] hover:dark:text-pink-500">
           <label htmlFor="user_email" className="mb-1">Email:</label>
           <input
             type="email"
             name="user_email"
             id="user_email"
-            className="border border-gray-300 dark:border-[#13ecd4] px-3 py-2 rounded-md text-slate-900 w-full"
+            className="border border-gray-300 dark:border-[#13ecd4] dark:hover:border-pink-500 px-3 py-2 rounded-md text-slate-900 w-full"
             required
           />
         </div>
-        <div className="flex flex-col mb-4 py-2 dark:text-[#13ecd4]">
+        <div className="flex flex-col mb-4 py-2 dark:text-[#13ecd4] hover:dark:text-pink-500">
           <label htmlFor="message" className="mb-1">Message:</label>
           <textarea
             name="message"
             id="message"
-            className="border border-gray-300 dark:border-[#13ecd4] px-3 py-2 rounded-md text-slate-900 w-full"
+            className="border border-gray-300 dark:border-[#13ecd4] dark:hover:border-pink-500 px-3 py-2 rounded-md text-slate-900 w-full"
             required
           ></textarea>
         </div>
