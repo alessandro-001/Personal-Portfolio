b5cd924e984c1aca47a1c3ac65009cebad6cb1c5
alessandro-001 <alessandrofrondini@gmail.com>
2025-01-30 12:58:23 +0000

    updated CV

--- src/components/CurriculumDownload.jsx
+++ src/components/CurriculumDownload.jsx
@@ -6,7 +6,7 @@ export default function CurriculumDownload() {
   return (
     <div className='flex items-center justify-center flex-col'>
         <p className="text-md md:text-xl text-slate-900 dark:text-gray-300">
-            Thank you for reaching until this point!
+            Thank you for reaching this point!
         </p>
         <a href={Curriculum} download="Alessandro Frondini CV">
             <button className="py-2 mx-5 mt-8 px-8 border border-slate-900 dark:border-[#13ecd4] dark:hover:border-pink-500 text-base font-medium rounded-md text-slate-900 dark:text-[#13ecd4] hover:bg-slate-900 dark:hover:text-pink-500 hover:text-white md:text-md text-center justify-center">
