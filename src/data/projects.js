715ef720c82185a5bd847f295ffa76ffb918febc
alessandro-001 <alessandrofrondini@gmail.com>
2025-02-11 16:20:20 +0000

    fixed img location

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,14 +3,14 @@ import WeatherOrNot from '../../src/assets/WeatherornotApp.png';
 import PomodoroImg from '../../src/assets/PomodoroImg.png';
 import QuiztopiaImg from '../../src/assets/QuiztopiaImg.png';
 import WeatherApp from '../../src/assets/WeatherAppMobile.png';
-import PortfolioImg from '../../src/assets/PortfolioImg.png';
+import PortfolioImage from '../../src/assets/PortfolioImage.png';
 import ApplianceList from '../../src/assets/applianceList.png';
 
 
 const works = [
   {
     title: 'Personal Portfolio',
-    imgUrl: PortfolioImg,
+    imgUrl: PortfolioImage,
     tech: ['Javascript', 'React', 'TailwindCSS'],
     description: 'This project showcases my web development work and skills. I created this portfolio using React and Vite.js. The UI is styled with Tailwind CSS. Explore my projects, skills, and experience in this visually appealing web page.',
     repolink: 'https://github.com/alessandro-001/Personal-Portfolio',
