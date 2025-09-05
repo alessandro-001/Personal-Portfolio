74e7fd8f8c82c7739cbf1c152fe7204097e5cffa
alessandro-001 <alessandrofrondini@gmail.com>
2025-09-05 20:44:25 +0100

    update cv

--- src/components/CurriculumDownload.jsx
+++ src/components/CurriculumDownload.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Curriculum from "../../src/assets/Alessandro-Frondini-DEVELOPER.pdf";
+import Curriculum from "../../src/assets/Alessandro Frondini_CV.pdf";
 
 
 export default function CurriculumDownload() {
